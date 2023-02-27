146957860e544a92180bba2dca68662256ae530c
pdbsy <124060188+pdbsy@users.noreply.github.com>
2023-02-27 18:11:29 -0500

    Add files via upload

---
+++ presentation/wk05/code/script.js
@@ -0,0 +1,6 @@
+
+  function showone() { 
+    console.log("click")
+    var div = document.getElementById("div"); 
+    div.innerHTML = "Personal Effects reminds us of the inevitable, elusive nature of poetic language. The subject is always already gone, beyond us, out of reach, and receding further. The subject is precisely that which evades. “Personal Effects” also reminds us of the powerful uses to which language is put by political forces. -- David Baker"; 
+  }
