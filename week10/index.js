9b1ca5b302f9419266c2b29104ccc6ada80e41f9
pdbsy <124060188+pdbsy@users.noreply.github.com>
2023-04-13 00:02:38 -0400

    Update index.js

--- week10/index.js
+++ week10/index.js
@@ -1,4 +1,3 @@
-// 祝词
 var words=[
     'G00d Morning',
     'MASSAGE',
@@ -37,13 +36,11 @@ var words=[
     'revolution',
     '@'];
 
-// 生成指定范围随机数（保留小数点后两位）
 function randomNum(min,max){
     var num = (Math.random() * (max - min +1 )+min).toFixed(2);
     return num;
 }
 
-//初始化函数
 function init(){
     var container = document.querySelector(".container");
     var f = document.createDocumentFragment();//创建文档片段对象
@@ -55,12 +52,10 @@ function init(){
         word.innerText=w;
         word.classList.add('word');
 
-        // 随机生成色相值（0为红色、120为绿色、240为蓝色、360为红色）
         var hue = randomNum(0,240);
         word.style.color='hsl('+hue+',100%,65%)';        
 
         word_box.classList.add("word-box");
-        // 生成随机数值，并赋值给自定义属性
         word_box.style.setProperty("--margin-top",randomNum(-40,20)+'vh');
         word_box.style.setProperty("--margin-left",randomNum(6,35)+'vw');
         word_box.style.setProperty("--animation-duration",randomNum(8,20)+'s');
@@ -72,5 +67,4 @@ function init(){
     container.appendChild(f);
 }
 
-// 绑定加载事件
 window.addEventListener('load',init)
